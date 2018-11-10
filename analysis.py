269e742f1a0ee55227b3bc456025f003903e2aa2
Gary.Z <dinner3000@hotmail.com>
2018-11-11 00:09:47 +0800

    feat: create CLI template files for data analysis

---
+++ analysis.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cleanse.py"""
+
+__author__ = 'Gary.Z'
+
+import os
+import click
+
+from data_cleansing.logging import *
+from data_analysis.reports_generator import *
+
+logger = get_logger(__name__)
+
+
+@click.command()
+# @click.argument('file', nargs=1)
+@click.option('--input-file', '-i', required=True, help='Input file path')
+@click.option('--output-folder', '-o', help='Output folder path')
+def main(input_file, output_folder):
+    """This script cleansing raw data into cleaned data."""
+
+    if not os.path.exists(input_file):
+        logger.error('input file [{}] not exist, quit'.format(input_file))
+        exit(0)
+
+    if output_folder is None:
+        dirpath, filename = os.path.split(input_file)
+        name, ext = os.path.splitext(filename)
+    else:
+        if not os.path.exists(output_folder):
+            logger.error('output path [{}] not exist, quit'.format(output_folder))
+            exit(0)
+        if not os.path.isdir(output_folder):
+            logger.error('output path [{}] is not dir, quit'.format(output_folder))
+            exit(0)
+
+        # call report generator class here
+        rg = ReportGenerator(input_file, output_folder)
+        rg.generate()
+        # or call generation function here
+        generate_reports(input_file, output_folder)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        # logger.error(e)
+    finally:
+        pass
---
+++ analysis_watch.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""cleanse.py"""
+
+__author__ = 'Gary.Z'
+
+import click
+import time
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from data_cleansing.logging import *
+from data_analysis.reports_generator import *
+
+logger = get_logger(__name__)
+
+
+class InputFileMatchingEventHandler(FileSystemEventHandler):
+
+    def __init__(self, batch_cleansing_handler, output_folder):
+        super().__init__()
+
+        self._batch_cleansing_handler = batch_cleansing_handler
+        self._output_folder = output_folder
+
+    def on_moved(self, event):
+        # super(LoggingEventHandler, self).on_moved(event)
+        #
+        # what = 'directory' if event.is_directory else 'file'
+        # logging.info("Moved %s: from %s to %s", what, event.src_path,
+        #              event.dest_path)
+        pass
+
+    def on_created(self, event):
+        # super(LoggingEventHandler, self).on_created(event)
+        #
+        # what = 'directory' if event.is_directory else 'file'
+        # logging.info("Created %s: %s", what, event.src_path)
+        filename = os.path.basename(event.src_path)
+        name, ext = os.path.splitext(filename)
+        if ext == '.xlsx':
+            logger.info('input file {} detected'.format(event.src_path))
+            self._batch_cleansing_handler(event.src_path, self._output_folder)
+        pass
+
+    def on_deleted(self, event):
+        # super(LoggingEventHandler, self).on_deleted(event)
+        #
+        # what = 'directory' if event.is_directory else 'file'
+        # logging.info("Deleted %s: %s", what, event.src_path)
+        pass
+
+    def on_modified(self, event):
+        # super(LoggingEventHandler, self).on_modified(event)
+        #
+        # what = 'directory' if event.is_directory else 'file'
+        # logging.info("Modified %s: %s", what, event.src_path)
+        pass
+
+
+def try_move_file(src, dst, max_retry=20, retry_interval=3):
+        success = False
+        retry_count = max_retry
+        while (not success) and retry_count > 0:
+            try:
+                shutil.move(src, dst)
+                success = True
+            except Exception as e:
+                logger.debug(e)
+                logger.info("waiting for file lock release")
+                time.sleep(retry_interval)
+            finally:
+                retry_count -= 1
+        if not success:
+            raise Exception('file is locked by another process and exceeded waiting time limit: {} secs'.format(retry_interval * max_retry))
+
+
+def run_generate_reports(input_file, output_folder):
+    filename = os.path.basename(input_file)
+    name, ext = os.path.splitext(filename)
+
+    backup_file = os.path.join(output_folder, filename)
+    try_move_file(input_file, backup_file)
+    input_file = backup_file
+
+    dirpath = output_folder
+
+    try:
+        # call report generator class here
+        rg = ReportGenerator(input_file, output_folder)
+        rg.generate()
+        # or call generation function here
+        generate_reports(input_file, output_folder)
+
+
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        with open(get_error_filename(dirpath, name), 'w') as f:
+            f.write(e.__str__())
+    finally:
+        pass
+
+
+@click.command()
+# @click.argument('file', nargs=1)
+@click.option('--input-folder', '-i', required=True, help='Input file path')
+@click.option('--output-folder', '-o', required=False, help='Output folder path')
+@click.option('--degree', '-d', help='Specify educational background, e.g. "本科毕业生"， "专科毕业生"')
+@click.option('--trace-mode', '-t', is_flag=True, type=bool, help='Trace mode will add additional comments for each rinsed cell')
+@click.option('--stream-mode', '-sm', is_flag=True, type=bool, help='Use stream mode to process data, or by default in memory')
+@click.option('--concurrent-mode', '-cm', is_flag=True, type=bool, help='Use multi-process to process data, or by default in serial')
+def main(input_folder, output_folder, degree, stream_mode, concurrent_mode, trace_mode):
+    """This script cleansing raw data into cleaned data."""
+
+    if not os.path.exists(input_folder):
+        logger.error('input path [{}] not exist, quit'.format(input_folder))
+        exit(0)
+
+    if not os.path.isdir(input_folder):
+        logger.error('input path [{}] is not folder, quit'.format(input_folder))
+        exit(0)
+
+    if output_folder is None:
+        output_folder = os.path.join(input_folder, 'cleaned')
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+
+    if not os.path.exists(output_folder):
+        logger.error('output path [{}] not exist, quit'.format(output_folder))
+        exit(0)
+
+    if not os.path.isdir(output_folder):
+        logger.error('output path [{}] is not folder, quit'.format(output_folder))
+        exit(0)
+
+    logger.info('program is running in watching mode, watch path \'{}\', press Control-C to stop'.format(input_folder))
+
+    event_handler = InputFileMatchingEventHandler(run_generate_reports, output_folder)
+    observer = Observer()
+    observer.schedule(event_handler, input_folder, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info('program stopped')
+        observer.stop()
+    except Exception as e:
+        logger.error(e)
+        observer.stop()
+    finally:
+        observer.join()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        # logger.error(e)
+    finally:
+        pass
+
