e5ab7f4bb0c0b474787590083b00141c04a05404
Gary.Z <dinner3000@hotmail.com>
2018-11-11 00:09:04 +0800

    refact: separate logger config

--- data_cleansing/config.py
+++ data_cleansing/config.py
@@ -5,7 +5,7 @@
 
 __author__ = 'Gary.Z'
 
-import logging
+from data_cleansing.logging import *
 
 EXCEL_INDEX_BASE = 1
 HEADER_ROW_INDEX = 0 + EXCEL_INDEX_BASE
@@ -180,26 +180,6 @@ RINSE_RULE_IRRELEVANT_QUESTIONS_V6_COMPATIBLE = [
      RINSE_RULE_KEY_OPERATOR: RINSE_RULE_OPERATOR_IN,
      RINSE_RULE_KEY_ACTION: ['B5', 'B6', 'B7-1', 'B7-2', 'B7-3', 'B7-4', 'B8', 'B9-1', 'B9-2']},
 ]
-
-
-def get_logger(name):
-    # formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s - %(message)s')
-    # formatter = logging.Formatter('%(asctime)s %(name)s %(thread)d(%(threadName)s) %(levelname)s - %(message)s')
-    formatter = logging.Formatter('%(asctime)s %(name)s %(process)d(%(processName)s) %(levelname)s - %(message)s')
-
-    file_handler = logging.FileHandler('runlog.txt', mode='a')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-    return logger
 
 
 # definition of question code mapping to excel column index
