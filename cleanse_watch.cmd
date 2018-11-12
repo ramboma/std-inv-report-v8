2fa42d137927adb2d82697fc71117d0eb2c7f4b1
Gary.Z <dinner3000@hotmail.com>
2018-11-12 14:14:38 +0800

    feat: add cmd file for starting watch

---
+++ cleanse_watch.cmd
@@ -0,0 +1,9 @@
+
+C:
+
+cd C:\service-deploy\apps\std-inv-report-v8
+
+
+cmd /k "activate python36 && python cleanse_watch.py -i C:\service-deploy\shared\std-inv-report-v8\raw -o C:\service-deploy\shared\std-inv-report-v8\cleaned -sm -cm"
+
+
