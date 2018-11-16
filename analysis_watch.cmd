e9e21247e716dbb6401eb52449e283bcba6962f4
Gary.Z <dinner3000@hotmail.com>
2018-11-16 11:14:42 +0800

    feat:add analysis watch cmd

---
+++ analysis_watch.cmd
@@ -0,0 +1,9 @@
+
+C:
+
+cd C:\service-deploy\apps\std-inv-report-v8
+
+
+cmd /k "activate python36 && python analysis_watch.py -i C:\service-deploy\shared\std-inv-report-v8\analysis\cleaned -o C:\service-deploy\shared\std-inv-report-v8\analysis\result"
+
+
