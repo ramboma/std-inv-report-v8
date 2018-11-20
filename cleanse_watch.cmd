4857b5abdde0dcc8ef17f1060673a6585a91feb1
Gary.Z <dinner3000@hotmail.com>
2018-11-20 20:36:02 +0800

    feat: add -cn arg to cleanse_watch

--- cleanse_watch.cmd
+++ cleanse_watch.cmd
@@ -4,6 +4,6 @@ C:
 cd C:\service-deploy\apps\std-inv-report-v8
 
 
-cmd /k "activate python36 && python cleanse_watch.py -i C:\service-deploy\shared\std-inv-report-v8\cleanse\raw -o C:\service-deploy\shared\std-inv-report-v8\cleanse\cleaned -sm -cm"
+cmd /k "activate python36 && python cleanse_watch.py -i C:\service-deploy\shared\std-inv-report-v8\cleanse\raw -o C:\service-deploy\shared\std-inv-report-v8\cleanse\cleaned -sm -cm -cn"
 
 
