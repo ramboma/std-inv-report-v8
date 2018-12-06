23d797e298681d44259e568c83eb4a315bef3de2
Gary.Z <dinner3000@hotmail.com>
2018-12-06 21:59:32 +0800

    feat: update salary rinse rule follow today's request

--- data_cleansing/salary_cleanser.py
+++ data_cleansing/salary_cleanser.py
@@ -16,7 +16,7 @@ class SalaryValueCollector:
     def __init__(self, log_handler=None):
         self._value_list = []
         # self.__np_value_list = None
-        self._top_n = 0
+        # self._top_n = 0
         self._lock_down = False
         self._mean = 0
         self._stdev = 0
@@ -44,21 +44,22 @@ class SalaryValueCollector:
     def get_higher_limit(self):
         if not self._lock_down:
             raise Exception('cannot operate on a non-lock-down instance')
-        if self._top_n > 0:
-            return self._value_list[self._top_n - 1]
-        else:
-            return sys.maxsize
+        # if self._top_n > 0:
+        #     return self._value_list[self._top_n - 1]
+        # else:
+        #     return sys.maxsize
+        return SALARY_FILTER_HIGHER_LIMIT
 
-    def get_top_n(self):
-        return self._top_n
+    # def get_top_n(self):
+    #     return self._top_n
 
     def lock_down(self):
         self._lock_down = True
-        self._top_n = round(self._value_list.__len__() * SALARY_FILTER_TOP_RATIO)
-
-        if self._top_n < 1:
-            # raise Exception('Unexpected top n < 1')
-            self._logger.warn('Unexpected top n({}) < 1'.format(self._top_n))
+        # self._top_n = round(self._value_list.__len__() * SALARY_FILTER_TOP_RATIO)
+        #
+        # if self._top_n < 1:
+        #     # raise Exception('Unexpected top n < 1')
+        #     self._logger.warn('Unexpected top n({}) < 1'.format(self._top_n))
 
         new_list = []
         self._value_list.sort(reverse=True)
@@ -90,14 +91,16 @@ class SalaryValueCollector:
         self._logger.info('salary collector result:')
         self._logger.info('>> total: {}'.format(self._value_list.__len__()))
         self._logger.info('>> lower limit: {}'.format(self.get_lower_limit()))
-        self._logger.info('>> top N: {}'.format(self.get_top_n()))
+        # self._logger.info('>> top N: {}'.format(self.get_top_n()))
         self._logger.info('>> higher limit: {}'.format(self.get_higher_limit()))
         self._logger.info('>> mean: {}'.format(self.get_mean()))
         self._logger.info('>> stdev: {}'.format(self.get_stdev()))
         self._logger.info('>> stdev*4: {}'.format(self.get_stdev_4()))
 
     def __str__(self):
-        return 'salary collector result: total: {}, lower limit: {}, top N: {}, higher limit: {}, mean: {}, stdev: {}, stdev*4: {}'\
-            .format(self._value_list.__len__(), self.get_lower_limit(), self.get_top_n(), self.get_higher_limit(), self.get_mean(), self.get_stdev(), self.get_stdev_4())
+        # return 'salary collector result: total: {}, lower limit: {}, top N: {}, higher limit: {}, mean: {}, stdev: {}, stdev*4: {}'\
+        #     .format(self._value_list.__len__(), self.get_lower_limit(), self.get_top_n(), self.get_higher_limit(), self.get_mean(), self.get_stdev(), self.get_stdev_4())
+        return 'salary collector result: total: {}, lower limit: {}, higher limit: {}, mean: {}, stdev: {}, stdev*4: {}'\
+            .format(self._value_list.__len__(), self.get_lower_limit(), self.get_higher_limit(), self.get_mean(), self.get_stdev(), self.get_stdev_4())
 
     __repr__ = __str__
