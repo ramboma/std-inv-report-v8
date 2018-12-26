405bbbc154e02d9743913879439bb09c25ecffcf
kuoren1 <panyulonga0@126.com>
2018-12-27 07:15:53 +0800

    add log when do report

--- data_analysis/analyze_run.py
+++ data_analysis/analyze_run.py
@@ -4,7 +4,9 @@ from data_analysis.data_calculate import *
 from data_analysis.data_style import *
 from data_analysis.result_write import *
 from data_analysis.data_analyze import *
+from data_cleansing.logging import *
 
+logger = get_logger(__name__)
 
 class AnalyzeRunner(object):
     def __init__(self, writer):
@@ -19,10 +21,13 @@ class AnalyzeRunner(object):
         try:
             result = analyzer.analyse()
             self._write.write_new_book('{}.xlsx'.format(name), result)
+            logger.info("{} report finished".format(name))
         except Exception as e:
             # log error message to log file
             # write user friendly error file
             # normal exit for next analyzer running
+            logger.info("{} report failed, fail reason:{}".format(name,str(e)))
+
             tip_file = os.path.join(self._write.folder, name + '.txt')
             with open(tip_file, 'w') as f:
                 f.write('{} 报表生成是发生错误，产生错误原因:{}'.format(name, str(e)))
